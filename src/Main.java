86d60175086f7c4b428110d4fecb04d61e9d32bb
Dmitry234 <g-d-a@bk.ru>
2021-12-20 11:22:36 +0700

    second branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Cat barsik = new Cat(3000, "Barsik", 5, "Yellow");
+        Cat barsik = new Cat(3100, "Barsik", 5, "Yellow");
         Cat bigCat = new Cat(5500, "Myrka", 2, "Black");
-        Cat smallCst = new Cat(1500, "Vasya", 3, "White");
+        Cat smallCst = new Cat(1500, "Vasya", 4, "White");
 
 
         }
