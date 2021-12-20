f67c3fdfdc78299becaf8acacd4dc0459f2ff6ba
Dmitry234 <g-d-a@bk.ru>
2021-12-20 11:20:51 +0700

    Main branch

---
+++ src/Cat.java
@@ -0,0 +1,24 @@
+public class Cat {
+
+        public double weight;
+        public String name;
+        public int age;
+        public String color;
+
+
+    public Cat(double weight, String name, int age, String color) {
+        this.weight = weight;
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+    public void meow(){
+        System.out.println("meow");
+    }
+    public void pee(){
+        weight -= 20;
+    }
+    public void eat(){
+        weight += 50;
+    }
+}
