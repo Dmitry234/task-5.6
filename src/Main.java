652f20590ce1c28d95b4bb343e634f2de13c6d97
Dmitry234 <95896074+Dmitry234@users.noreply.github.com>
2022-02-04 10:30:56 +0700

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Cat barsik = new Cat(3000, "Barsik", 5, "Yellow");
-        Cat bigCat = new Cat(5500, "Myrka", 2, "Black");
-        Cat smallCst = new Cat(1500, "Vasya", 3, "White");
-
-
-        }
-    }
